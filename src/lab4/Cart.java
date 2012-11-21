7267c25485fda403ba0d52febe22dbbd4ba7eb21
Lawrence Siebert <gryftir@gmail.com>
2012-11-20 18:26:54 -0800

    added Cart constructor, and method to return the number of items

--- src/lab4/Cart.java
+++ src/lab4/Cart.java
@@ -1,11 +1,22 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class Cart {
+	ArrayList<Item> cartitems;
 
-	
 	public static void main(String[] args) {
-
 
 	}
 
+	public Cart(int itemnum) {
+		this.cartitems = new ArrayList<Item>();
+		for (int i = 0; i < itemnum; i++) {
+			this.cartitems.add(new Item());
+		}
+	}
+	public int size()
+	{
+		return cartitems.size();
+	}
 }
