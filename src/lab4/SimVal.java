b769020bfe0eca78aa0c3a4aed809bdacf9028c3
Lawrence Siebert <gryftir@gmail.com>
2012-11-20 17:31:09 -0800

    created SimVal to store values for the simulation

---
+++ src/lab4/SimVal.java
@@ -0,0 +1,17 @@
+package lab4;
+
+public class SimVal {
+
+	public int lanenum;
+	public int expresslanenum;
+	public int expresslaneitemlimit;
+	public int startofrange;
+	public int endofrange;
+	public int customersnum;
+	public int cartsizemax;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
