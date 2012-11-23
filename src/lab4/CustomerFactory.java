d527551a009d0af80c9a0fc068fe485e0b54293c
Lawrence Siebert <gryftir@gmail.com>
2012-11-22 16:44:16 -0800

    added random number generator, made constructor, and wrote madeCustomer method that generates a random cart size

--- src/lab4/CustomerFactory.java
+++ src/lab4/CustomerFactory.java
@@ -1,14 +1,23 @@
 package lab4;
+import java.util.Random;
 
-public class CustomerFactory extends Customer {
+public class CustomerFactory {
 
-	/**
-	 * @param args
-	 */
+	private SimVal values;
+	private Random generator;
 	
 	public static void main(String[] args) {
-		
 
 	}
 
+	public CustomerFactory(SimVal val) {
+		this.values = val;
+		this.generator =  new Random();
+	}
+	
+
+	protected Customer makeCustomer()
+	{
+		return new Customer(this.generator.nextInt(values.cartsizemax) + 1);
+	}
 }
