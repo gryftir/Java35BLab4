b768b900e276b07b47e7c8cdea8973489030602b
Lawrence Siebert <gryftir@gmail.com>
2012-11-22 18:14:46 -0800

    clarified start and end of range meant express lane limit on items

--- src/lab4/SimVal.java
+++ src/lab4/SimVal.java
@@ -4,9 +4,9 @@ public class SimVal {
 
 	public int lanenum;
 	public int expresslanenum;
-	public int expresslaneitemlimit;
-	public int startofrange;
-	public int endofrange;
+	public int expresslaneitemlimit; 
+	public int startofrange; // express lane item limit
+	public int endofrange; //express lane item limit
 	public int customersnum;
 	public int cartsizemax;
 	public static void main(String[] args) {
