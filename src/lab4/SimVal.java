e55c76cae694ab196cb93e2ee1031894d4a21333
Lawrence Siebert <gryftir@gmail.com>
2012-11-24 14:45:10 -0800

    Added constructors, including a copy Constructor, since java's clone method apparently makes a shallow copy.

--- src/lab4/SimVal.java
+++ src/lab4/SimVal.java
@@ -11,6 +11,33 @@ public class SimVal {
 	public int cartsizemax;
 	public static void main(String[] args) {
 	}
-
+	public SimVal clone()
+	{
+		return new SimVal(this);
+	}
+	protected SimVal(int lanenum, int expresslanenum, int expresslaneitemlimit,
+			int startofrange, int endofrange, int customersnum, int cartsizemax) {
+		this.lanenum = lanenum;
+		this.expresslanenum = expresslanenum;
+		this.expresslaneitemlimit = expresslaneitemlimit;
+		this.startofrange = startofrange;
+		this.endofrange = endofrange;
+		this.customersnum = customersnum;
+		this.cartsizemax = cartsizemax;
+	}
+	protected SimVal(SimVal val)
+	{
+		this.lanenum = val.lanenum;
+		this.expresslanenum = val.expresslanenum;
+		this.expresslaneitemlimit = val.expresslaneitemlimit;
+		this.startofrange = val.startofrange;
+		this.endofrange = val.endofrange;
+		this.customersnum = val.customersnum;
+		this.cartsizemax = val.cartsizemax;
+	}
+	public SimVal()
+	{
+	}
+	
 }
 //note can use .clone() method to make a hard copy
